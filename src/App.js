6aa9631074104b3e09ce82278582a007d46ad1c2
Tapomoy Das <160303114+TAPOMOY1994@users.noreply.github.com>
2024-07-20 06:16:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import "./App.css";
-import LoginPage from "./LoginPage/LoginPage";
-import { Switch, Route } from "react-router-dom";
-import HeaderArea from "./HeaderArea/HeaderArea";
-import SignIn from "./Googlesignin/SignIn";
-
-
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import LoginPage from './LoginPage/LoginPage';
+import HeaderArea from './HeaderArea/HeaderArea';
+import SignIn from './Googlesignin/SignIn';
+import './App.css';
 
 function App() {
-
-  return <>
-     
-    <div className="App">
-      <Switch>
-      <Route path="/signin" component={SignIn} />
-        <Route path="/LoginPage" exact={true} component={LoginPage} />
-        <Route path="/HeaderArea" component={HeaderArea} />
-      </Switch>
-    </div>
-
-
-</>;
-
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/signin" component={SignIn} />
+          <Route path="/HeaderArea" component={HeaderArea} />
+          <Route path="/" exact component={LoginPage} />
+          {/* Redirect any other routes to the login page */}
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
